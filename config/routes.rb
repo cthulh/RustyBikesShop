ef4cd7ea590ef2bb844e24d309b0eeb9e0bcce24
cthulh <tryglavson@hotmail.co.uk>
2016-06-04 19:53:02 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   post '/thank_you', to: 'static_pages#thank_you'
   get '/featured', to: 'static_pages#landing_page'
   root 'static_pages#index'
