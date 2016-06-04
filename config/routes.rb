a9b2f951fde6c56105dcd4df57f89094d17c1839
cthulh <tryglavson@hotmail.co.uk>
2016-06-04 18:41:52 +0100

    Thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
-  post 'static_pages/thank_you'
   get 'static_pages/contact'
+  post '/thank_you', to: 'static_pages#thank_you'
   get '/featured', to: 'static_pages#landing_page'
   root 'static_pages#index'
 
