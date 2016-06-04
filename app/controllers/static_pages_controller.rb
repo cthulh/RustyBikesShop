add12ce4d1bc8f408083c08b079d0d8e2a14b82e
cthulh <tryglavson@hotmail.co.uk>
2016-06-04 19:00:47 +0100

    Action Mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,12 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(from: @email,
+      to: 'tryglavson@hotmail.co.uk',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
   end
 end
