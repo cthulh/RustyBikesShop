7ff29568fa63bd452afddaa4dc8810245147ef77
cthulh <tryglavson@hotmail.co.uk>
2016-06-05 14:52:16 +0100

    User model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522131926) do
+ActiveRecord::Schema.define(version: 20160605135000) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id",    limit: 4
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20160522131926) do
     t.datetime "updated_at",                               null: false
     t.string   "colour",      limit: 255
     t.decimal  "price",                     precision: 10
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
