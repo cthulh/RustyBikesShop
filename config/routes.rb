e3f0e88d30e0d46b53a36565756a7bccc8d077a4
cthulh <tryglavson@hotmail.co.uk>
2016-06-06 10:13:49 +0100

    Login and sign up pages plus styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,23 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  post '/thank_you', to: 'static_pages#thank_you'
-  get '/featured', to: 'static_pages#landing_page'
+
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  post 'thank_you' => 'static_pages#thank_you'
+  get 'featured' => 'static_pages#landing_page'
   root 'static_pages#index'
+
+  as :user do
+  	get 'login' => 'devise/sessions#new'
+  end
+
+  as :user do
+  	get 'logout' => 'devise/sessions#destroy'
+  end
+
+  as :user do
+  	get 'sign_up' => 'devise/registrations#new'
+  end
 
 end
