9d8a28087a4d90c9c64303c8771ec1251ecede2a
cthulh <tryglavson@hotmail.co.uk>
2016-06-10 22:18:10 +0100

    Role based authorisation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :manage, User, id: user.id
+    can :manage, Order, id: user.id
+	can :read, Product
+	can [:update, :create, :destroy], Product if user.role == "admin"
+    can :manage, :all if user.role == "admin"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605142342) do
+ActiveRecord::Schema.define(version: 20160610203912) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id",    limit: 4
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160605142342) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "role",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
