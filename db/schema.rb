0b51b752fedf5d988504530923bcd7716800684c
cthulh <tryglavson@hotmail.co.uk>
2016-06-11 18:34:03 +0100

    Comments model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610203912) do
+ActiveRecord::Schema.define(version: 20160611172655) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.text     "body",       limit: 65535
+    t.integer  "rating",     limit: 4
+    t.integer  "product_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["product_id"], name: "index_comments_on_product_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id",    limit: 4
@@ -53,4 +65,6 @@ ActiveRecord::Schema.define(version: 20160610203912) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
