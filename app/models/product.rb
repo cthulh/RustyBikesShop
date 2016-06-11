f8ca3e9c1a2a46c35a82844adbe02d8e322c4db7
cthulh <tryglavson@hotmail.co.uk>
2016-06-11 23:05:11 +0100

    Product class method - not a controller action

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
 	has_many :orders
 	has_many :comments
+
+	 # Calculating an average rating for a product for the #show
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
