9cfd3a6ad7bdc5ae0ae28729112b1767b3a6779b
cthulh <tryglavson@hotmail.co.uk>
2016-07-23 16:14:41 +0100

    Using factories

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,15 +5,14 @@ describe Product do
 	context "when the product has comments" do
 
 		before do
-			@product = Product.create!(name: "race bike", description: "brown", colour: "brown", price: 1.2)
-			@user = User.create!(first_name: "john", last_name: "john", email: "jojo@jojo.jo", password: "jojojo666")
-			@product.comments.create!(rating: 1, user: @user, body: "Terrible bike!")
-			@product.comments.create!(rating: 3, user: @user, body: "Average bike!")
-			@product.comments.create!(rating: 5, user: @user, body: "Superb bike!")
+			@product1 = FactoryGirl.create(:product)
+			comment1 = FactoryGirl.create(:comment, product: @product1)
+			comment2 = FactoryGirl.create(:comment, rating: 3, product: @product1)
+			comment3 = FactoryGirl.create(:comment, rating: 5, product: @product1)
 		end
 
 		it "return an average rating of all comments" do
-			expect(@product.average_rating).to eq 3
+			expect(@product1.average_rating).to be==3
 		end
 
 		it "is not valid" do
